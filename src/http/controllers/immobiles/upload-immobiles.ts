9a4aa36a2687702222d5cc896de88911b2afe01c
carlosgodoi <edu-godoi@hotmail.com>
2024-11-08 14:58:28 -0300

    refactor: Ajustando diretorio de upload da imagem

--- src/http/controllers/immobiles/upload-immobiles.ts
+++ src/http/controllers/immobiles/upload-immobiles.ts
@@ -1,39 +1,13 @@
 import { makeUploadImmobilelUseCase } from "@/use-cases/factories/make-upload-immobile-use-case";
 import cloudinary from "@/utils/cloudinary";
 import { FastifyReply, FastifyRequest } from "fastify";
-import fs from "fs";
 import { Readable } from "stream";
-
-import { pipeline } from "stream/promises";
 
 export async function uploadImmobile(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
   const { id } = request.params as { id: string };
-  // try {
-  //   const parts = request.files({ limits: { fileSize: 100 * 1024 * 1024 } });
-  //   const images: { path: string }[] = [];
-
-  //   for await (const part of parts) {
-  //     const fileName = `${id}-${Date.now()}-${part.filename}`;
-  //     await pipeline(
-  //       part.file,
-  //       fs.createWriteStream(`public/images/immobiles/${fileName}`)
-  //     );
-  //     images.push({ path: `public/images/immobiles/${fileName}` });
-  //   }
-
-  //   const uploadUseCase = makeUploadImmobilelUseCase();
-
-  //   await uploadUseCase.execute({
-  //     id,
-  //     images,
-  //   });
-  //   return reply.status(200).send();
-  // } catch (error) {
-  //   return reply.status(500).send(error);
-  // }
 
   try {
     const parts = request.files({ limits: { fileSize: 100 * 1024 * 1024 } });
@@ -42,7 +16,7 @@ export async function uploadImmobile(
     for await (const part of parts) {
       const stream = cloudinary.uploader.upload_stream(
         {
-          folder: "immobiles", // Defina a pasta no Cloudinary
+          folder: "imoveis", // Defina a pasta no Cloudinary
           public_id: `${id}-${Date.now()}-${part.filename}`,
         },
         (error, result) => {
@@ -50,7 +24,9 @@ export async function uploadImmobile(
           images.push({ path: result ? result.secure_url : '' }); // Armazena o URL seguro da imagem no Cloudinary
         }
       );
-      Readable.from(part.file).pipe(stream); // Envia o arquivo para o Cloudinary
+
+      // Convertemos a parte recebida para um stream legível para o Cloudinary
+      Readable.from(part.file).pipe(stream);
     }
 
     const uploadUseCase = makeUploadImmobilelUseCase();
